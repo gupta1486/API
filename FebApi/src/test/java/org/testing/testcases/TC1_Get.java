6364a341f07a4c5aa81c4e434153bb44c4e9a5ce
Sumit <Sumit@LAPTOP-OPMRHBQN>
2023-04-14 15:33:07 +0530

    get file

--- FebApi/src/test/java/org/testing/testcases/TC1_Get.java
+++ FebApi/src/test/java/org/testing/testcases/TC1_Get.java
@@ -1,6 +1,11 @@
 package org.testing.testcases;
 import static io.restassured.RestAssured.*;
 
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
@@ -14,17 +19,39 @@ public class TC1_Get {
 		//.get("http://localhost:3000/APIStudentsList?id=1");
 		//or 2nd way to fine id 
 		.get("http://localhost:3000/APIStudentsList");
-		//or third Way
-			//String id =res.jsonPath().get("[4].id");
-		     //System.out.println(id);
-		System.out.println("status code is "+res.statusCode());
-		System.out.println("Response Data is ");
-		System.out.println(res.asString());
-//		
-//		String[] name= {"sumit","test"};
-//		int s=name.length;
-//				System.out.println(s);
+//or third Way
+			String id =res.jsonPath().get("[4].id");
+		     System.out.println(id);
+		//System.out.println("status code is "+res.statusCode());
+		//System.out.println("Response Data is ");
+		//System.out.println(res.asString());
 		
+//***** Fetch single value of perticular field
+		JSONArray js = new JSONArray(res.asString());
+		JSONObject jo= js.getJSONObject(3);// 0 is for index of the object
+		System.out.println("Fetch single value of perticular field" + jo.get("id"));
+		
+//get multiple occurrence of a key
+		
+		for(int i=0;i<js.length();i++)
+		{
+			JSONObject j= js.getJSONObject(i);
+			//System.out.println("get multiple occurrence of a key");
+			System.out.println("get multiple occurrence of a key " +  j.get("name"));
+		}
+
+//Extract all the key value of an object	
+		
+		JSONObject O= js.getJSONObject(14);
+	System.out.println("Extract all the key value of an object " + O.keySet());
+	
+	
+	Set<String> ALLKey = O.keySet();
+	for(String key:ALLKey)
+	{
+System.out.println("Extract all the key value of an object " + key +"=" + O.get(key));
+	}
+			
 	}
 
 }
